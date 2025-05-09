cbe1d8976aa4fc9bfd3d7fc3fd31cf040adcb494
rennoank <rennoank@gmail.com>
2025-05-09 01:47:21 -0300

    Atualização de um outro programinha em Spring Boot

---
+++ src/main/java/oi/github/Cadastro/Java/CadastroJavaApiApplication.java
@@ -0,0 +1,13 @@
+package oi.github.Cadastro.Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroJavaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oi/github/Cadastro/Java/CadastroJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package oi.github.Cadastro.Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
